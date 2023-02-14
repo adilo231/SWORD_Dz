3e0d9bc20d8ae0f3f447ec54a301236454883246
hosni adil imad eddine <33214519+adilo231@users.noreply.github.com>
2023-02-14 14:31:18 +0100

    Add parallel computing

--- HISBmodel.py
+++ HISBmodel.py
@@ -4,17 +4,37 @@ import pandas as pd
 import random 
 import matplotlib.pyplot as plt
 import matplotlib
+import multiprocessing
+import time
+
+
 plt.style.use('ggplot')
 
 
+def DisplyResults(dfs):
+    fig, axe= plt.subplots(2,3)
+    
+    # 
+    # for i, ax in enumerate(axe.flat):
+    #     
 
+    for df_rep in dfs:
+        df = df_rep.recv()
+        col=df.columns
+        for i, ax in enumerate(axe.flat):
+            ax.plot(df[col[i]])
+            ax.set_title(f'The evolution of {col[i]}')
+            ax.set_ylabel(f'Number of {col[i]}')
+            ax.set_xlabel(f'Time')
 
+    plt.show()
 
 
 
 
 class HSIBmodel():
     def __init__(self,Graph,Seed_Set,opinion_set,baisAccepte=0.3, setptime=0.125, Probability=0.3,Tdet=np.Infinity,method='none'):
+        
         self.setptime=setptime
         self.Graph=Graph
         self.time=0.125
@@ -22,11 +42,11 @@ class HSIBmodel():
         self.ListInfectedNodes=Seed_Set
         RumorPopularity=self.SetParameters(opinion_set)
         self.baisAccepte=baisAccepte
-        self.Statistical=pd.DataFrame(data={'NonInfected': self.Nbr_nonInfected,
+        self.Statistical=pd.DataFrame(data={'Non_Infected': self.Nbr_nonInfected,
                                        'Infected': self.Nbr_Infected,
                                        'Spreaders': self.Nbr_Infected,
-                                       'OpinionDenying': self.OpinionDenying,
-                                       'OpinionSupporting': self.OpinionSupporting,
+                                       'Opinion_Denying': self.OpinionDenying,
+                                       'Opinion_Supporting': self.OpinionSupporting,
                                        'RumorPopularity': RumorPopularity
                                        },index=[0])
 
@@ -68,7 +88,7 @@ class HSIBmodel():
           
         return RumorPopularity
 
-    def updateOpinion(self,id,jugf,NegR,R): 
+    def UpdateOpinion(self,id,jugf,NegR,R): 
         opinion=jugf
         assert R>0
         if NegR != 0:
@@ -88,7 +108,8 @@ class HSIBmodel():
         else:
             self.OpinionDenying+=1 
 
-    def simulation(self):
+    def runModel(self,i=0,send_end=0):
+        print(f'Simulation number {i} is on run')
         time=0.125
         
         while self.ListInfectedNodes:  
@@ -106,7 +127,7 @@ class HSIBmodel():
                     ActualAttraction = np.exp(-RelativeTime * self.Graph.nodes[id]['beta']) * np.abs(np.sin((RelativeTime * self.Graph.nodes[id]['omega'] )+ self.Graph.nodes[id]['delta']))
                     RumorPopularity += ActualAttraction * self.Graph.degree(id)
                     #Update node opinion
-                    self.updateOpinion( id,
+                    self.UpdateOpinion( id,
                                         self.Graph.nodes[id]['jug'],
                                         self.Graph.nodes[id]['Accp_NegR'],
                                         self.Graph.nodes[id]['AccpR'])
@@ -155,17 +176,38 @@ class HSIBmodel():
                               
         
         #save each step to send it to viewing later
-            new =pd.DataFrame(data={'NonInfected': self.Nbr_nonInfected ,
-                                        'Infected': self.Nbr_Infected,
-                                        'Spreaders': Nbr_Spreaders,
-                                        'OpinionDenying': self.OpinionDenying,
-                                        'OpinionSupporting': self.OpinionSupporting,
-                                        'RumorPopularity': RumorPopularity
+            new =pd.DataFrame(data={'Non_Infected': self.Nbr_nonInfected,
+                                       'Infected': self.Nbr_Infected,
+                                       'Spreaders': Nbr_Spreaders,
+                                       'Opinion_Denying': self.OpinionDenying,
+                                       'Opinion_Supporting': self.OpinionSupporting,
+                                       'RumorPopularity': RumorPopularity
                                         },index=[time])
             self.Statistical =pd.concat([self.Statistical, new])      
-            time += self.setptime;   
+            time += self.setptime; 
+        print(f'Simulation number {i} has finnied')
+        if send_end!=0:  
+            send_end.send(self.Statistical)
 
+def Simulations(NbrSim,g,seedNode,seedOpinion):
+    jobs = []
+    pipe_list = []
+    sim=HSIBmodel(g,seedNode,seedOpinion)
+    for i in range(NbrSim):
+        recv_end, send_end = multiprocessing.Pipe(False)
+        p = multiprocessing.Process(target=sim.runModel, args=(i,send_end))
+        jobs.append(p)
+        pipe_list.append(recv_end)
+        
+    for proc in jobs:
+        proc.start()
+    for proc in jobs:
+        proc.join()
+    # result_list = [x.recv() for x in pipe_list]
+    # print (result_list)
+    return pipe_list
 
+    
  
 # Crete Random graphe
 def CreateGraph(parameters,N=100,M=3):
@@ -174,7 +216,7 @@ def CreateGraph(parameters,N=100,M=3):
     return g
 
 # Init the model paramters 
-def getRandomValues(n,min,max):
+def GetRandomValues(n,min,max):
     return (np.random.rand(n)*(max - min)) + min
 
 def InitParameters(g,parameters):
@@ -182,17 +224,17 @@ def InitParameters(g,parameters):
 
     # Set omega
     
-    values = dict( enumerate(getRandomValues(n,parameters['omega_min'],parameters['omega_max'])) )
+    values = dict( enumerate(GetRandomValues(n,parameters['omega_min'],parameters['omega_max'])) )
     nx.set_node_attributes(g, values, 'omega')
     # Set beta
-    values = dict( enumerate(getRandomValues(n,parameters['beta_min'],parameters['beta_max'])) )
+    values = dict( enumerate(GetRandomValues(n,parameters['beta_min'],parameters['beta_max'])) )
     nx.set_node_attributes(g, values, 'beta')
     # Set delta
-    values = dict( enumerate(getRandomValues(n,parameters['delta_min'],parameters['delta_max'])) )
+    values = dict( enumerate(GetRandomValues(n,parameters['delta_min'],parameters['delta_max'])) )
     nx.set_node_attributes(g, values, 'delta')
 
     # Set jug
-    values = dict( enumerate(getRandomValues(n,parameters['jug_min'],parameters['jug_max'])) )
+    values = dict( enumerate(GetRandomValues(n,parameters['jug_min'],parameters['jug_max'])) )
     nx.set_node_attributes(g, values, 'jug')
 
     # Set other Attributes
@@ -210,31 +252,48 @@ if __name__ == '__main__':
 
    
     #Graph's Parametres 
-    n=200
+    n=10000
     P=0.3
     K=100
     M=20
     nbb=0
+    NbrSim=10
    
     parameters={'omega_min':np.pi/24,
                 'omega_max':np.pi*2,
                 "delta_min":np.pi/24,
                 "delta_max":np.pi/2,
                 "jug_min":0.1,
-                "jug_max":0.9,
+                "jug_max":0.4,
                 "beta_max":1.2,
                 "beta_min":0.2}
-
+    print('graphe generation')
     g= CreateGraph(parameters,n)
-    seed= int(0.01*n)
+    seed= int(0.05*n)
     l= ['D','S']
     seedNode=random.sample(range(0,n),seed)
     seedOpinion=random.choices(l,k=seed)
-    print(seedNode,seedOpinion,len(seedNode),len(seedOpinion))
-    print(g.nodes[0]['jug'])
-    sim=HSIBmodel(g,seedNode,seedOpinion)
-    sim.simulation()
-    sim.DisplyResults()
+    print('simulation')
+    
+    # print(seedNode,seedOpinion,len(seedNode),len(seedOpinion))
+    # print(g.nodes[0]['jug'])
+    # sim=HSIBmodel(g,seedNode,seedOpinion)
+    # sim.runModel()
+    # sim.DisplyResults()
+
+    start_time = time.time()  
+    dfs=Simulations(10,g,seedNode,seedOpinion)
+    end_time = time.time()  
+    print('Parallel time: ',end_time-start_time)
+    DisplyResults(dfs)
+    
+    # for x in pipe_list:
+    #     DisplyResults()
+    #  print((x.recv().shape))
+
+
+
+ 
    
     
 
