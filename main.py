9a2b1c9e59c4f7c1c443a262698763cf1fb9e263
aymanboulma <95140765+aymanboulma@users.noreply.github.com>
2023-02-20 00:43:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,38 +20,8 @@ if __name__ == '__main__':
     M = 20
     nbb = 0
     NbrSim = 50
+    type=1
 
-    # parameters = {'omega_min': np.pi/24,
-    #               'omega_max': np.pi*2,
-    #               "delta_min": np.pi/24,
-    #               "delta_max": np.pi/2,
-    #               "jug_min": 0.1,
-    #               "jug_max": 0.4,
-    #               "beta_max": 1.2,
-    #               "beta_min": 0.05}
-    # print('graphe generation')
-    # g = CreateGraph(parameters, n)
-    # seed = int(0.05*n)
-    # l = ['D', 'S']
-    # seedNode = random.sample(range(0, n), seed)
-    # seedOpinion = random.choices(l, k=seed)
-    # print('simulation')
-
-    # run simple simulation and display
-    # print(seedNode,seedOpinion,len(seedNode),len(seedOpinion))
-    # print(g.nodes[0]['jug'])
-    # sim=HSIBmodel(g,seedNode,seedOpinion)
-    # sim.runModel()
-    # sim.DisplyResults()
-
-    # # Run multiple and paralle simulations than display
-    # start_time = time.time()
-    # dfs = Simulations(3, g, seedNode, seedOpinion, 1)
-    # end_time = time.time()
-    # print('Parallel time: ', end_time-start_time)
-    # DisplyResults(dfs)
-
-    #Run multiple and paralle simulations get final results
     start_time = time.time()
     parameters = {'omega_min': np.pi/24,
                   'omega_max': np.pi*2,
@@ -62,15 +32,21 @@ if __name__ == '__main__':
                   "beta_max": 1.2,
                   "beta_min": 0.8}
     SimulationResults= pd.DataFrame()
-    for beta in np.arange(0.1,1,0.05):
-        parameters['beta_min']=beta
-        parameters['beta_max']=beta+0.1
-        g = gg.CreateGraph(parameters, n)
-        results = m.Simulations(5, g,seedsSize=0.05, typeOfSim= 2)
-        SimulationResults = m.CreateDataFrame(results,SimulationResults,sim=f'beta=[{beta},{beta+0.1}]')
-        
-    end_time = time.time()
-    print('Parallel time: ', end_time-start_time)
+    
+    if(type==1):
+      g = gg.CreateGraph(parameters, 1000,10)
+      results = m.Simulations(5, g,seedsSize=0.05, typeOfSim= type)
+      m.Create_Data_Globale(results)
+    else:
+        for beta in np.arange(0.1,1,0.05):
+            parameters['beta_min']=beta
+            parameters['beta_max']=beta+0.1
+            g = gg.CreateGraph(parameters, 1000,10)
+            results = m.Simulations(5, g,seedsSize=0.05, typeOfSim= 2)
+            SimulationResults = m.CreateDataFrame(results,SimulationResults,sim=f'beta=[{beta},{beta+0.1}]')
+            
+        end_time = time.time()
+        print('Parallel time: ', end_time-start_time)
     
     fig, axes = plt.subplots(3, 1, figsize=(11, 10), sharex=True)
     for name, ax in zip(['Infected','Suporting','Denying'], axes):
