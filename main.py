601b84bab9676bea4438a4057e69310f9d75a92a
hosni adil imad eddine <33214519+adilo231@users.noreply.github.com>
2023-02-20 13:47:26 +0100

    last update simulatore

--- main.py
+++ main.py
@@ -15,47 +15,52 @@ if __name__ == '__main__':
     M = 20
     nbb = 0
     NbrSim = 50
-    type=1
 
-    start_time = time.time()
+    # parameters = {'omega_min': np.pi/24,
+    #               'omega_max': np.pi*2,
+    #               "delta_min": np.pi/24,
+    #               "delta_max": np.pi/2,
+    #               "jug_min": 0.1,
+    #               "jug_max": 0.4,
+    #               "beta_max": 1.2,
+    #               "beta_min": 0.05}
+    # print('graphe generation')
+    # g = CreateGraph(parameters, n)
+    # seed = int(0.05*n)
+    # l = ['D', 'S']
+    # seedNode = random.sample(range(0, n), seed)
+    # seedOpinion = random.choices(l, k=seed)
+    # print('simulation')
+
+    # run simple simulation and display
+
+    # sim=HSIBmodel(g,seedNode,seedOpinion)
+    # sim.runModel()
+    # sim.DisplyResults()
     parameters = {'omega_min': np.pi/24,
                   'omega_max': np.pi*2,
                   "delta_min": np.pi/24,
                   "delta_max": np.pi/2,
                   "jug_min": 0.7,
                   "jug_max": 0.99,
-                  "beta_max": 1.2,
-                  "beta_min": 0.8}
-    SimulationResults= pd.DataFrame()
-    
-    if(type==1):
-      g = gg.CreateGraph(parameters, 1000,10)
-      results = m.Simulations(5, g,seedsSize=0.05, typeOfSim= type)
-      m.Create_Data_Globale(results)
-    else:
-        for beta in np.arange(0.1,1,0.05):
-            parameters['beta_min']=beta
-            parameters['beta_max']=beta+0.1
-            g = gg.CreateGraph(parameters, 1000,10)
-            results = m.Simulations(5, g,seedsSize=0.05, typeOfSim= 2)
-            SimulationResults = m.CreateDataFrame(results,SimulationResults,sim=f'beta=[{beta},{beta+0.1}]')
-            
-        end_time = time.time()
-        print('Parallel time: ', end_time-start_time)
-    
-        fig, axes = plt.subplots(3, 1, figsize=(11, 10), sharex=True)
-        for name, ax in zip(['Infected','Suporting','Denying'], axes):
-            sns.boxplot(data=SimulationResults, x='sim', y=name, ax=ax)
-            ax.set_ylabel('Number of individuals')
-            ax.set_title(name)
-        # Remove the automatic x-axis label from all but the bottom subplot
-        if ax != axes[-1]:
-            ax.set_xlabel('')
-        plt.show()
-   
+                  "beta_max": 0.6,
+                  "beta_min": 0.1}
+    # # Run multiple and paralle simulations than display
+    Generator=gg.CreateSytheticGraph()
+    Simulator = sim.RumorSimulator()
 
-    # Get the DataFrame results from simulation
-    # for x in pipe_list:
+    g = Generator.CreateGraph(parameters,graphModel='AB',Graph_size=n)     
+    start_time = time.time()
+    df=pd.DataFrame()
+    for i in tqdm(range(0,5)):
 
-    #  print((x.recv().shape))
-    
+        Generator.InitParameters(g,parameters)
+        aux = Simulator.runSimulation(g, NbrSim=50 ,seedsSize=0.05, typeOfSim=2,simName=f'sim{i}')
+        df=pd.concat([df,aux])
+
+    end_time = time.time()
+    print('Parallel time: ', end_time-start_time)
+
+
+    Simulator.DisplyResults( df,resultType=2)
+    print(df)
