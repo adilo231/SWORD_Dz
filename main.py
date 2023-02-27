d1813cc7e3244b959360977fadcec21ce6f68b27
hosni adil imad eddine <33214519+adilo231@users.noreply.github.com>
2023-02-27 08:20:13 +0100

    hosni changes

--- main.py
+++ main.py
@@ -1,20 +1,60 @@
 import DataStorage.GraphGenerator as gg
+from DataExtraction.TwitterExtractor import  TweetExtractor
 import Model.Simulator as sim
 import numpy as np
 import pandas as pd
 import time
 from tqdm import  tqdm
 
+# Twitter API credentials
+API_credentials={
+'consumer_key' : "2nyGsL1XyRXFGMw0MXbvm7cnY",
+'consumer_secret' : "SPTXJkrZfukdZ4gnhYZVgOmpvEbE9Z1lKwxjRHOxbbucidENzs",
+'access_key' : "2275955796-bcNPjJsfAWdzG1dJrd7AqjzoWgsEP83XgxYNIaq",
+'access_secret' : "IhzSm6k7GyqndpqrSPgypQDIm93uMjaBRkRw431TABLSE"}
 
+
+
+# MongoDB credentials
+mongo_uri = "mongodb://localhost:27017/"
+
+# Neo4j credentials
+neo_uri = "bolt://localhost:7687"
+neo_user = "neo4j"
+neo_password = "admin"
+
+
+
+
+
+
+# define rate limit handler function
 if __name__ == '__main__':
+    Extractor =TweetExtractor(mongo_uri,neo_uri,neo_user,neo_password,API_credentials)
+    Query={
+        'query' : "تبون",
+        'lang': "en"
 
+    }
+    
+
+    mongo_db = "twitter_db"
+    mongo_tweet_collection = "Teboune"
+    mongo_user_collection = "Teboune_users"
+    # Extractor.Topic_Tweet_Extraction( Query,mongo_db,mongo_tweet_collection,mongo_user_collection)
+
+
+    Locations=[ 'Algérie','Algiers','Alger','Algeria']
+    mongo_db = "twitter_db"
+    mongo_user = "AlgeriaTwitterGraph"
+    Extractor.Graph_Extraction(mongo_db,mongo_user,Locations)
     # Graph's Parametres
-    n = 300
-    P = 0.3
-    K = 100
-    M = 20
-    nbb = 0
-    NbrSim = 50
+    # n = 300
+    # P = 0.3
+    # K = 100
+    # M = 20
+    # nbb = 0
+    # NbrSim = 50
 
     # parameters = {'omega_min': np.pi/24,
     #               'omega_max': np.pi*2,
@@ -36,31 +76,31 @@ if __name__ == '__main__':
 
     # sim=HSIBmodel(g,seedNode,seedOpinion)
     # sim.runModel()
-    # sim.DisplyResults()
-    parameters = {'omega_min': np.pi/24,
-                  'omega_max': np.pi*2,
-                  "delta_min": np.pi/24,
-                  "delta_max": np.pi/2,
-                  "jug_min": 0.7,
-                  "jug_max": 0.99,
-                  "beta_max": 0.6,
-                  "beta_min": 0.1}
+    # # sim.DisplyResults()
+    # parameters = {'omega_min': np.pi/24,
+    #               'omega_max': np.pi*2,
+    #               "delta_min": np.pi/24,
+    #               "delta_max": np.pi/2,
+    #               "jug_min": 0.7,
+    #               "jug_max": 0.99,
+    #               "beta_max": 0.6,
+    #               "beta_min": 0.1}
     # # Run multiple and paralle simulations than display
-    Generator=gg.CreateSytheticGraph()
-    Simulator = sim.RumorSimulator()
+    # Generator=gg.CreateSytheticGraph()
+    # Simulator = sim.RumorSimulator()
 
-    g = Generator.CreateGraph(parameters,graphModel='AB',Graph_size=n)     
-    start_time = time.time()
-    df=pd.DataFrame()
-    for i in tqdm(range(0,5)):
+    # g = Generator.CreateGraph(parameters,graphModel='AB',Graph_size=n)     
+    # start_time = time.time()
+    # df=pd.DataFrame()
+    # for i in tqdm(range(0,5)):
 
-        Generator.InitParameters(g,parameters)
-        aux = Simulator.runSimulation(g, NbrSim=50 ,seedsSize=0.05, typeOfSim=2,simName=f'sim{i}')
-        df=pd.concat([df,aux])
+    #     Generator.InitParameters(g,parameters)
+    #     aux = Simulator.runSimulation(g, NbrSim=50 ,seedsSize=0.05, typeOfSim=2,simName=f'sim{i}')
+    #     df=pd.concat([df,aux])
 
-    end_time = time.time()
-    print('Parallel time: ', end_time-start_time)
+    # end_time = time.time()
+    # print('Parallel time: ', end_time-start_time)
 
 
-    Simulator.DisplyResults( df,resultType=2)
-    print(df)
+    # Simulator.DisplyResults( df,resultType=2)
+    # print(df)
