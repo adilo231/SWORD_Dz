5b486098250eaf568ea5cbf7adb68ce0cd3adead
hosni adil imad eddine <33214519+adilo231@users.noreply.github.com>
2023-03-13 08:48:41 +0100

    hosni

--- DataExtraction/TwitterExtractor.py
+++ DataExtraction/TwitterExtractor.py
@@ -6,7 +6,22 @@ import pickle
 import os
 from tqdm import tqdm
 def Algerian_location(location):
-    Locations=[ 'Algérie','Algiers','Alger','Algeria','الجزائر','dz','bladi','setif']
+    Locations=[ 'Algérie','Algiers','Alger','Algeria','الجزائر','dz','Tipaza','bladi','setif','Oran',
+                'Adrar', 'أدرار', 'Chlef', 'الشلف', 'Laghouat', 'الأغواط', 'Oum El Bouaghi', 'أم البواقي', 
+                'Batna', 'باتنة', 'Béjaïa', 'بجاية', 'Biskra', 'بسكرة', 'Béchar', 'بشار', 'Blida', 'البليدة', 
+                'Bouira', 'البويرة', 'Tamanrasset', 'تمنراست', 'Tébessa', 'تبسة', 'Tlemcen', 'تلمسان', 'Tiaret',
+                'تيارت', 'Tizi Ouzou', 'تيزي وزو', 'Alger', 'الجزائر', 'Djelfa', 'الجلفة', 'Jijel', 'جيجل', 
+                'Sétif', 'سطيف', 'Saïda', 'سعيدة', 'Skikda', 'سكيكدة', 'Sidi Bel Abbès', 'سيدي بلعباس', 'Annaba', 
+                'عنابة', 'Guelma', 'قالمة', 'Constantine', 'قسنطينة', 'Médéa', 'المدية', 'Mostaganem', 'مستغانم', 
+                'M\'Sila', 'المسيلة', 'Mascara', 'معسكر', 'Ouargla', 'ورقلة', 'Oran', 'وهران', 'El Bayadh', 'البيض', 
+                'Illizi', 'إليزي', 'Bordj Bou Arréridj', 'برج بوعريريج', 'Boumerdès', 'بومرداس', 'El Tarf', 
+                'الطارف', 'Tindouf', 'تندوف', 'Tissemsilt', 'تيسمسيلت', 'El Oued', 'الوادي', 'Khenchela', 'خنشلة', 
+                'Souk Ahras', 'سوق أهراس', 'Tipaza', 'تيبازة', 'Mila', 'ميلة', 'Aïn Defla', 'عين الدفلى', 'Naâma', 
+                'النعامة', 'Aïn Témouchent', 'عين تموشنت', 'Ghardaïa', 'غرداية', 'Relizane', 'غليزان']
+
+
+                
+
     if location in Locations:
         return True
     for loc in Locations:
@@ -150,29 +165,29 @@ class TweetExtractor():
                     type=file.split('_')[1]
                     with open(f'{root}/{file}', 'rb') as f:
                         ids = pickle.load(f)
-                    print(user_id,type)
+                    print(user_id,type,)
                     if type=="friend":
                         with self.graphDB_Driver.driver.session() as session:
                             for friend_id in tqdm(ids):
-                                    friend_id = str(friend_id)
-                                    result = session.run("MERGE (a:User {id_str: $user_id}) "
-                                            "MERGE (b:User {id_str: $friend_id ,MongoCol: $mongo_col, Checked: $checked}) "
+                                    
+                                    result = session.run("MATCH (a:User {id_str: $user_id}) "
+                                            "MERGE (b:User {id_str: $friend_id , Checked: $checked}) "
                                             "MERGE (a)-[:FOLLOWS]->(b)", 
-                                            user_id=str(user_id), friend_id=str(friend_id),mongo_col=mongo_user_collection,checked=False)
+                                            user_id=(user_id), friend_id=str(friend_id),checked=False)
 
                     elif type=="follower":
                         with self.graphDB_Driver.driver.session() as session:
                             for follower_id in tqdm(ids):
-                                follower_id = str(follower_id)
+                                
                                 result = session.run("MATCH (a:User {id_str: $user_id}) "
-                                                    "MERGE (b:User {id_str: $follower_id,MongoCol: $mongo_col, Checked: $checked}) "
+                                                    "MERGE (b:User {id_str: $follower_id, Checked: $checked}) "
                                                     "MERGE (b)-[:FOLLOWS]->(a)", 
-                                                    user_id=str(user_id), follower_id=str(follower_id),mongo_col=mongo_user_collection,checked=False)
+                                                    user_id=(user_id), follower_id=str(follower_id),checked=False)
                 print("all Ids has been uploaded in the file: ",f'{root}/{file}', 'file remove it')
                 os.remove(f'{root}/{file}') 
             else:
-                 print("No data to be Uploaded sleeping for 1 min")
-                 time.sleep(60)               
+                 print("No data to be Uploaded sleeping for 5 mins")
+                 time.sleep(60*5)               
 
     def Graph_Extraction(self,mongo_db,mongo_user_collection,verbose=False):
        
@@ -181,22 +196,19 @@ class TweetExtractor():
         user_collection = db[mongo_user_collection]
 
                 
-        query = "MATCH (u:User {Checked: True,LocationChecked:True}) where u.followers_count>50000 RETURN u.id_str AS id,u.screen_name as name"
-
+        query = "MATCH (u:User {Checked: False})   RETURN u.id_str AS id"
+        #query="MATCH (u:User) WHERE u.cursor_followers <> 0 AND u.cursor_followers <> -1 RETURN u.id_str as id "
+        #query="MATCH (p:User{Checked: False})-[r:FOLLOWS]->({id_str:$id})RETURN p.id_str as id "
         # Retrieve user IDs from Neo4j that hasn't been checked
-        result = self.graphDB_Driver.session.run(query)
+        result = self.graphDB_Driver.session.run(query,id="1239152434309738496")
         user_ids = [record["id"] for record in result]
         if verbose:
             print("Number of users to be checked: ",len(user_ids))
         random.shuffle(user_ids)
-        print(len(user_ids))
-        ids_added=["2515500817","2912608949","4867041051","916563468882530305","714213541646102529","11714622", "1308128283331121154","1109777841682608129","224788463"]
-        user_ids = [user_id for user_id in user_ids if user_id not in ids_added]
-        print(len(user_ids),len(ids_added))
-        # user_ids=['2515500817']
+        
         # Iterate over user IDs
         for user_id in user_ids:
-            
+            print()
             try:
                 # Retrieve user's  from Twitter API
                 user = self.api.get_user(user_id)._json
@@ -207,99 +219,159 @@ class TweetExtractor():
                 else:
                     
                     if verbose:
-                        print('Already Added',user['screen_name'],user['id'])
+                        print('\tAlready Added',user['screen_name'],user['id'])
                 
                 Algerian =Algerian_location(user['location']) 
-                followerExtractered=user['followers_count']> 2000
+                
                 if verbose:
-                    print(f"get folower of user', {user['screen_name']},{user['id']} location {user['location']} : {Algerian} : {user['followers_count']> 2000}")
+                    print(f"Extracting user', {user['screen_name']},{user['id']} location: {user['location']} Is it: {Algerian} ")
                 with self.graphDB_Driver.driver.session() as session:
                     session.run(""" MATCH (u:User {id_str: $user_id})
                             SET u.screen_name=$screen_name,  
-                                u.id_str= $id_str,
                                 u.followers_count= $followers_count,
-                                u.location= $location,
                                 u.friends_count= $friends_count,
-                                u.MongoCol= $mongo_col,
+                                u.location= $location,
                                 u.Checked= $checked,
-                                u.LocationChecked=$LocationChecked,
-                                u.followerExtractered=$followerExtractered 
-                            RETURN u""",
-                            
-                            user_id=str(user['id']),
+                                u.Algerian=$Algerian,
+                                u.cursor_friends = COALESCE(u.cursor_friends, -1),
+                                u.cursor_followers = COALESCE(u.cursor_followers, -1)
+                               
+                            """,
+                            user_id=(user_id),
                             screen_name=user['screen_name'],
-                            id_str=user['id_str'],
                             followers_count=user['followers_count'],
-                            location=user['location'],
                             friends_count=user['friends_count'],
-                            mongo_col=mongo_user_collection,
-                            checked=True,
-                            LocationChecked=Algerian,
-                            followerExtractered=followerExtractered
+                            location=user['location'],
+                            checked=False,
+                            Algerian=Algerian,
+
                             )
                 
-                if (Algerian and user['followers_count']>= 2000 ):
+                if (Algerian):
                     # Get Friends IDs
                     if verbose:
                         print(f"\t Extracting friends and followers of user_id: {user_id} of a location: {user['location']} ")
                         print(f"\t\t getting friend: {user['friends_count']}")
+                        
+                    with self.graphDB_Driver.driver.session() as session:
+                        results=session.run("""  MATCH (n{id_str: $user_id}) RETURN n.cursor_friends """,user_id= str(user_id))
+                        cursor=results.single()[0]
 
-                    butch_number=0
-                    for friend_ids in tweepy.Cursor(self.api.friends_ids, user_id=user_id,count=500).pages():
+                    butch_number=1
+                    friend_ids = []
+                    
+                    while cursor != 0 and user['friends_count']>0:
+                        friend_ids = []
+                        print(f"\t\t\t cursor {cursor}")
+                        time.sleep(1)
+                        resutls=tweepy.Cursor(self.api.friends_ids,cursor=cursor, user_id=user_id,count=500).pages()
+                        page = next(resutls)
+                        cursor = resutls.next_cursor
+                        with self.graphDB_Driver.driver.session() as session:
+                            session.run(""" Merge (u:User {id_str: $user_id})
+                                            SET 
+                                            u.cursor_friends=$cursor 
+                                            RETURN u""",
+                                            user_id= str(user_id),
+                                            cursor=cursor
+                                            )
+                        friend_ids.extend(page)
+                        print(f"\t\t\t cursor {cursor}")
+                       
                         if verbose:
-                            print(f"\t\t\t{butch_number} -> {len(friend_ids)} extracted ready to load to DB")
+                            print(f"\t\t\t{butch_number}/{round(len(friend_ids)/500)+1} -> {len(friend_ids)} extracted ready to load to DB")
                         with open(f"Data/cache/{user_id}_friend_{butch_number}.pkl", 'wb') as f:
                                 pickle.dump(friend_ids, f)
                         butch_number+=1
-                        # for friend_id in friend_ids:
-                        #     try:
-                        #         friend_id = str(friend_id)
-                        #         with self.graphDB_Driver.driver.session() as session:
-                        #             result = session.run("MERGE (a:User {id: $user_id}) "
-                        #                 "MERGE (b:User {id: $friend_id ,MongoCol: $mongo_col, Checked: $checked}) "
-                        #                 "MERGE (a)-[:FOLLOWS]->(b)", 
-                        #                 user_id=str(user_id), friend_id=str(friend_id),mongo_col=mongo_user_collection,checked=False)
+                        for friend_id in friend_ids:
+                            try:
+                                
+                                with self.graphDB_Driver.driver.session() as session:
+                                    result = session.run("MERGE (a:User {id_str: $user_id}) "
+                                        "MERGE (b:User {id_str: $friend_id }) "
+                                        "MERGE (a)-[:FOLLOWS]->(b)"
+                                        "SET  b.Checked = COALESCE(b.Checked, false)", 
+                                        user_id=str(user_id), friend_id=str(friend_id))
 
-                        #     except tweepy.TweepError as e:
-                        #         print(f"Error fetching friends/followers of user {user_id}: {str(e)}")
-                        #         if "Rate limit exceeded" in str(e):
-                        #             print("Waiting for rate limit to reset...")
-                        #             time.sleep(60 * 15) # wait for 15 minutes
+                            except tweepy.TweepError as e:
+                                print(f"Error fetching friends/followers of user {user_id}: {str(e)}")
+                                if "Rate limit exceeded" in str(e):
+                                    print("Waiting for rate limit to reset...")
+                                    time.sleep(60 * 15) # wait for 15 minutes
                         if verbose:
                             print(f"\t\t\t{len(friend_ids)} id friends has been added")
                     if verbose:
                                 print(f"\t\t Extracting friend Ids end")           
                                 print(f"\t\t Extracting followers Ids: {user['followers_count']}")
+                    
+                    
+                    with self.graphDB_Driver.driver.session() as session:
+                        results=session.run("""  MATCH (n{id_str: $user_id}) RETURN n.cursor_followers """,user_id= str(user_id))
+                        cursor=results.single()[0]
+
+                    
                     butch_number=1
                     # Get Followers IDs
-                    for follower_ids in tweepy.Cursor(self.api.followers_ids, user_id=user_id,count=500).pages():
+                    while cursor != 0 and user['followers_count']>0:
+                        follower_ids = []
+                        print(f"\t\t\t cursor {cursor}")
+
+
+
+                        resutls= tweepy.Cursor(self.api.followers_ids,cursor=cursor,  user_id=user_id,count=500).pages()
+                        time.sleep(1)
+                        page = next(resutls)
+                        cursor = resutls.next_cursor
+                        with self.graphDB_Driver.driver.session() as session:
+                            session.run(""" Merge (u:User {id_str: $user_id})
+                                            SET 
+                                            u.cursor_followers=$cursor 
+                                            RETURN u""",
+                                            user_id= str(user_id),
+                                            cursor=cursor
+                                            )
+                        follower_ids.extend(page)
+                        print(f"\t\t\t cursor {cursor}")
+                        
                         if verbose:
-                            print(f"\t\t\t {butch_number}/{round(user['followers_count']/500)} ->  {len(follower_ids)} ID extracted ready to load to DB")
+                            print(f"\t\t\t {butch_number}/{round(user['followers_count']/500)+1} ->  {len(follower_ids)} ID extracted ready to load to DB")
                         with open(f"Data/cache/{user_id}_follower_{butch_number}.pkl", 'wb') as f:
                                 pickle.dump(follower_ids, f)
                         butch_number+=1
-                        # for follower_id in follower_ids:
-                        #     try:
-                        #         follower_id = str(follower_id)
-                        #         with self.graphDB_Driver.driver.session() as session:
-                        #             result = session.run("MERGE (a:User {id: $user_id}) "
-                        #                                 "MERGE (b:User {id: $follower_id,MongoCol: $mongo_col, Checked: $checked}) "
-                        #                                 "MERGE (b)-[:FOLLOWS]->(a)", 
-                        #                                 user_id=str(user_id), follower_id=str(follower_id),mongo_col=mongo_user_collection,checked=False)
-                        #     except tweepy.TweepError as e:
-                        #         print(f"Error fetching friends/followers of user {user_id}: {str(e)}")
-                        #         if "429" in str(e):
-                        #             print("Waiting for rate limit to reset...")
-                        #             time.sleep(60 * 15) # wait for 15 minutes
+                        for follower_id in follower_ids:
+                            try:
+                                  
+                                
+                                with self.graphDB_Driver.driver.session() as session:
+                                    result = session.run("MERGE (a:User {id_str: $user_id}) "
+                                                        "MERGE (b:User {id_str: $follower_id}) "
+                                                        "MERGE (a)<-[:FOLLOWS]-(b)"
+                                                        "SET  b.Checked = CASE WHEN b.Checked IS NULL THEN false ELSE b.Checked END", 
+                                                        user_id=str(user_id), follower_id=str(follower_id))
+                            except tweepy.TweepError as e:
+                                print(f"Error fetching friends/followers of user {user_id}: {str(e)}")
+                                if "429" in str(e):
+                                    print("Waiting for rate limit to reset...")
+                                    time.sleep(60 * 15) # wait for 15 minutes
                         if verbose:
                             print(f"\t\t\t{len(follower_ids)} ID extracted and has been added to BD")
-                    if verbose:            
-                                print(f"\t\t Followers Ids extraction ends")
+                        if verbose:            
+                                    print(f"\t\t Followers Ids extraction ends")
             except tweepy.TweepError as e:
                         print(f"Error fetching friends/followers of user {user_id}: {str(e)}")
                         if "429" in str(e):
                             print("Waiting for rate limit to reset...")
                             time.sleep(60 * 15) # wait for 15 minutes
+            with self.graphDB_Driver.driver.session() as session:
+                            session.run(""" Merge (u:User {id_str: $user_id})
+                                            SET 
+                                            u.Checked= $checked,
+                                            u.followerExtractered=$followerExtractered 
+                                            RETURN u""",
+                                            user_id= str(user_id),
+                                            checked=True,
+                                            followerExtractered=True
+                                            )
 
 
 
