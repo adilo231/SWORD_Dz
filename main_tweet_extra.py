e388127c61055864ba4bc31d43f5f78c9f3e58b9
Doumaz-Abderrahmane <100124179+Doumaz-Abderrahmane@users.noreply.github.com>
2023-03-21 10:09:34 +0100

    Update main_tweet_extra.py

--- main_tweet_extra.py
+++ main_tweet_extra.py
@@ -2,10 +2,10 @@
 from DataExtraction.TwitterExtractor import  TweetExtractor
 # Twitter API credentials
 API_credentials={
-'consumer_key' : "NlCb4KrL6LlB8HVB3RGYLy0lU",
-'consumer_secret' : "GvACIXrd7jWvNWnIg7geb8gfL80j0L7OuTmmHCLl5DAUMDLwHb",
-'access_key' : "1512829229356011530-CTLalKDfZPkJDPI2EfmG3CebbMXfDh",
-'access_secret' : "w8ndvJ0FMK2qyzwzjqRBZ2ca57PAcho4QC8v2Xty6lQ35"}
+'consumer_key' : "dna9eBEhIe9oVIisEyUc8ZsHX",
+'consumer_secret' : "0J7W6APSraCL4sJWvLSJyCGVOuHb5lPKyRK4NijZQQr0o0sSDm",
+'access_key' : "2275955796-2kLj1PkrA1gfI3xffissxqYlTdHbibeafRsSMKb",
+'access_secret' : "zbE7p91v1jGfXYXjA50PVwur9gGFVYNZkZ7cKKL7lAWvq"}
 
 
 
@@ -25,28 +25,48 @@ neo_password = "admin"
 # define rate limit handler function
 if __name__ == '__main__':
 
-
-    
-    Extractor =TweetExtractor(2)
+    Extractor =TweetExtractor(2)  
     Query={}
-    # Query['query'] = """( الجزائر OR Algérie OR algerie OR #Algérie OR ALGERIA OR Algeria) AND (fiat OR Fiat OR FIAT OR voiture OR #Fiat  OR فيات)"""
-    # Query['query'] = """ (التمور AND الجزائرية ) OR
-    #                       (#المغاربة_يشوهون_التمور_الجزائرية) OR
-    #                       (#التمور_الجزائرية ) """
+    
 
-    Query['query'] = """ Mahrez"""
-                      
- 
-    Query['lang']='*'
+
+    start_date = '2023-01-01'
+    end_date = '2023-03-21'
+    keywords = ["الجزائر" , "cycling" , "tour" , "#Algérie" , "ALGERIA" , "Algeria", "#Algeria","velo",'accident velo',"الدراجات"]
+    keyword_query = ' OR '.join(keywords)
+    Query['query'] = f" since:{start_date} until:{end_date} ({keyword_query})"
+    #Query['query']="""Tour d'Algerie International de Cyclisme 2023"""
+
+        
+    
+
+    #Query['lang']='*'
     mongo_db = "twitter_db"
-    mongo_tweet_collection = 'Mahrez'
+    mongo_tweet_collection = 'تظاهرة الدراجات الجزائر'
     mongo_user = f"AlgeriaTwitterGraph"
     Extractor.Topic_Tweet_Extraction( Query,mongo_db,mongo_tweet_collection,mongo_user)
 
+    
+    # Extractor =TweetExtractor(2)
+    # Query={}
+    # # Query['query'] = """( الجزائر OR Algérie OR algerie OR #Algérie OR ALGERIA OR Algeria) AND (fiat OR Fiat OR FIAT OR voiture OR #Fiat  OR فيات)"""
+    # # Query['query'] = """ (التمور AND الجزائرية ) OR
+    # #                       (#المغاربة_يشوهون_التمور_الجزائرية) OR
+    # #                       (#التمور_الجزائرية ) """
+
+    # Query['query'] = """tour de velo algerien"""
+                      
+ 
+    # Query['lang']='*'
+    # mongo_db = "twitter_db"
+    # mongo_tweet_collection = 'تظاهرة الدراجات الجزائر'
+    # mongo_user = f"AlgeriaTwitterGraph"
+    # Extractor.Topic_Tweet_Extraction( Query,mongo_db,mongo_tweet_collection,mongo_user)
+
    
-    query="MATCH (p:User{Checked: false})-[r:FOLLOWS]->({id_str:$id})RETURN p.id_str as id "
-    # Retrieve user IDs from Neo4j that hasn't been checked
-    Extractor.Graph_Extraction(mongo_db,mongo_user,query,verbose=True)
+    # query="MATCH (p:User{Checked: false})-[r:FOLLOWS]->({id_str:$id})RETURN p.id_str as id "
+    # # Retrieve user IDs from Neo4j that hasn't been checked
+    # Extractor.Graph_Extraction(mongo_db,mongo_user,query,verbose=True)
 
 
 
