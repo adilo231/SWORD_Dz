2e240bdf75500803f798847d886997f64fd41dfa
hosni adil imad eddine <33214519+adilo231@users.noreply.github.com>
2023-04-03 17:57:35 +0100

    hosni

--- mainDoumaz.py
+++ mainDoumaz.py
@@ -7,21 +7,21 @@ client_mongo = "mongodb://localhost:27017/"
 db_name = "twitter_db"
 
 
-collection_names='Mahrez'
+collection_names='Attaf'
 
 
 transforms=transform(db_name)
 
-    # pipline of transformers
-    transforms.pipeline(collection_names,remove_null=False,cloud_words=False,lang_dist=False,date_dist=False,stance_dist=False,localisation_dist=True)
+# pipline of transformers
+transforms.pipeline(collection_names,remove_null=True,cloud_words=True,lang_dist=True,date_dist=True,stance_dist=True,localisation_dist=True,verbose=True)
 
 
-    # #META dATA
-    # #NUMBER OF DOCS PER COLLECTION
-    # transforms.nbr_doc_per_collection()
-    # #META LANG REPARTITION
-    # transforms.meta_tweets_lang_repartition(verbose=True)
+# #META dATA
+# #NUMBER OF DOCS PER COLLECTION
+# transforms.nbr_doc_per_collection()
+# #META LANG REPARTITION
+# transforms.meta_tweets_lang_repartition(verbose=True)
 
-    
 
-    plt.show()
+
+plt.show()
