2042e7d83eac33a868fd083fe9ecd2d274eb3366
hosni adil imad eddine <33214519+adilo231@users.noreply.github.com>
2023-04-17 10:40:59 +0100

    hosni

--- mainDoumaz.py
+++ mainDoumaz.py
@@ -1,27 +1,27 @@
 from Transformer.transformerrs import *
+from Transformer.TextPreprocessing import *
 
-# Connect to MongoDB
-client_mongo = "mongodb://localhost:27017/"
 #
 # Select the database and collection
 db_name = "twitter_db"
 
 
-collection_names='Attaf'
+collection_names='Prix'
 
 
 transforms=transform(db_name)
 
-# pipline of transformers
-transforms.pipeline(collection_names,remove_null=True,cloud_words=True,lang_dist=True,date_dist=True,stance_dist=True,localisation_dist=True,verbose=True)
+# # pipline of transformers
+# transforms.pipeline(collection_names,remove_null=True,cloud_words=True,lang_dist=True,date_dist=True,stance_dist=True,localisation_dist=True,verbose=True)
 
-transforms.stance_language_repartition(collection_names,verbose=True)
+# transforms.stance_language_repartition(collection_names,verbose=True)
 # #META dATA
 # #NUMBER OF DOCS PER COLLECTION
 # transforms.nbr_doc_per_collection()
 # #META LANG REPARTITION
 # transforms.meta_tweets_lang_repartition(verbose=True)
 
+transforms.Topic_detection(collection_names,verbose=True)
 
 
-plt.show()
+
