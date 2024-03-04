de844ac1574cfce1ca596853d4c00447fd1b1076
Phant5m <147447693+Phant5m@users.noreply.github.com>
2024-03-04 09:47:45 +0100

    Create Download_arabic_model.py

---
+++ Transformer/Classifier/sentiment_analysis/Download_arabic_model.py
@@ -0,0 +1,15 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+# Load the tokenizer
+tokenizer = AutoTokenizer.from_pretrained("nourmorsy/PermoBERT-Arabic-Sentiment-Analysis-Farasa-BPE-44000Token")
+
+# Load the model
+model = AutoModelForSequenceClassification.from_pretrained("nourmorsy/PermoBERT-Arabic-Sentiment-Analysis-Farasa-BPE-44000Token")
+
+# Define the directory where you want to save the model
+save_directory = "./Models/Arabic"
+
+# Save the tokenizer and model
+tokenizer.save_pretrained(save_directory)
+model.save_pretrained(save_directory)
