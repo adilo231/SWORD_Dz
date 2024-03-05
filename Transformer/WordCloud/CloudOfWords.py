8ba2c117bc33e0d4313cc9d9ca7326d9ea64bfd1
Phant5m <147447693+Phant5m@users.noreply.github.com>
2024-03-05 03:36:10 +0100

    Update CloudOfWords.py

--- Transformer/WordCloud/CloudOfWords.py
+++ Transformer/WordCloud/CloudOfWords.py
@@ -3,17 +3,30 @@ from collections import Counter
 import arabic_reshaper
 from bidi.algorithm import get_display
 import matplotlib.pyplot as plt
+import re
 
 class CloudOfWords:
-    def __init__(self):
+    def __init__(self,database,collection):
+        self.docdbh= DocDBHandler(database,collection)
+        self.tweets=self.docdbh.GetDocuments()
         self.wordcloud=None
-    def generateCW(self, list_of_tokens, language,path_arabic_font=None):
+        self.tokens=None
+    def generateCW(self,language,path_arabic_font=None):
+        self.tokens = []
+        for tweet in tweets :
+            try:
+                if tweet['lang']==language :
+                    self.tokens.extend(tweet['tokens'])
+            except:
+                pass
+        #this line is to remove non textual chars
+        self.tokens = [re.sub(r'[^\w\s]', '', token) for token in self.tokens]
         if path_arabic_font != None :
            self.path_arabic_font=path_arabic_font
         if language == 'arabic':
-            list_of_tokens_reshaped = [get_display(arabic_reshaper.reshape(token)) for token in list_of_tokens]
+            list_of_tokens_reshaped = [get_display(arabic_reshaper.reshape(token)) for token in self.tokens]
         else:
-            list_of_tokens_reshaped = list_of_tokens
+            list_of_tokens_reshaped = self.tokens
 
 
         word_freq = Counter(list_of_tokens_reshaped)
@@ -23,11 +36,10 @@ class CloudOfWords:
                                   font_path=self.path_arabic_font).generate_from_frequencies(word_freq)
         else:
             self.wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(word_freq)
-        
-        return self.wordcloud
 
     def showCW(self):
         plt.figure(figsize=(10, 5))
         plt.imshow(self.wordcloud, interpolation='bilinear')
         plt.axis('off')
         plt.show()
+
