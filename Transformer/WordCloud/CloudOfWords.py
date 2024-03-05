450572f687bcc1666218779022af31657c4db04d
Phant5m <147447693+Phant5m@users.noreply.github.com>
2024-03-05 09:31:13 +0100

    Update CloudOfWords.py

--- Transformer/WordCloud/CloudOfWords.py
+++ Transformer/WordCloud/CloudOfWords.py
@@ -1,17 +1,21 @@
-from wordcloud import WordCloud
+from wordcloud import WordCloud , STOPWORDS
 from collections import Counter
 import arabic_reshaper
 from bidi.algorithm import get_display
 import matplotlib.pyplot as plt
 import re
-from DataStorage.DocDBHandler import DocDBHandler 
+
 class CloudOfWords:
     def __init__(self,database,collection):
         self.docdbh= DocDBHandler(database,collection)
         self.tweets=self.docdbh.GetDocuments()
         self.wordcloud=None
         self.tokens=None
-    def generateCW(self,language,path_arabic_font=None):
+    def generateCW(self,language,path_arabic_font=None,max_words=None):
+        if max_words == None :
+            self.max_words=200
+        else : 
+            self.max_words=max_words
         self.tokens = []
         for tweet in tweets :
             try:
@@ -19,13 +23,16 @@ class CloudOfWords:
                     self.tokens.extend(tweet['tokens'])
             except:
                 pass
-        #this line is to remove non textual chars
-        self.tokens = [re.sub(r'[^\w\s]', '', token) for token in self.tokens]
-        if path_arabic_font != None :
-           self.path_arabic_font=path_arabic_font
+        
+
         if language == 'ar':
+            if path_arabic_font != None :
+                self.path_arabic_font=path_arabic_font
+            self.tokens=[token for token in self.tokens if re.match(r'^[\u0600-\u06FF\s]+$', token)]
             list_of_tokens_reshaped = [get_display(arabic_reshaper.reshape(token)) for token in self.tokens]
         else:
+            #this line is to remove non textual chars
+            self.tokens = [re.sub(r'[^\w\s]', '', token) for token in self.tokens]
             list_of_tokens_reshaped = self.tokens
 
 
@@ -33,7 +40,7 @@ class CloudOfWords:
 
         if language == 'ar':
             self.wordcloud = WordCloud(width=800, height=400, background_color='white',
-                                  font_path=self.path_arabic_font).generate_from_frequencies(word_freq)
+                                  font_path=self.path_arabic_font,max_words = self.max_words, stopwords = set(STOPWORDS)).generate_from_frequencies(word_freq)
         else:
             self.wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(word_freq)
 
