7787cbbb20a17ac0095b7a3d4fb90d87a9541b7e
Phant5m <147447693+Phant5m@users.noreply.github.com>
2024-03-18 02:37:20 +0100

    Update test.py

--- DataExtraction/test.py
+++ DataExtraction/test.py
@@ -1,10 +1,10 @@
 #### to test fb extractor
-
 query="ramadan"
 max_posts=10
 type_target="search"
 type_data="posts"
-num_comments=0
 fb=FacebookExtractor()
 fb.search(query,type_target,type_data,max_posts,num_comments)
 print(fb.view_posts())
+
+#### 
