14d886da97beb8a56279e72493b1434831c848bf
Phant5m <147447693+Phant5m@users.noreply.github.com>
2024-03-18 02:17:32 +0100

    Update test.py

--- DataExtraction/test.py
+++ DataExtraction/test.py
@@ -4,6 +4,6 @@ query="ramadan"
 max_posts=10
 type_target="search"#type_target="profil"
 type_data="posts"  #type_data="posts"      #comments=posts+comments
-fb=FacebookExtractor(fb.driver)
+fb=FacebookExtractor()
 fb.search(query,type_target,type_data)
 fb.posts
